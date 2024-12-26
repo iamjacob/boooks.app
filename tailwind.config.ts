7cb3c39fcbb0018142ef09d3199fe5dd4313454d
jacob gawad <iam@jacobg.me>
2024-12-26 15:21:49 +0100

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,28 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        accent: 'var(--accent-color)',
+        text: 'var(--text-color)',
+        
+        /* extra */
+        subtext: 'var(--subtext-color)',
+        border: 'var(--border-color)',
+        code: 'var(--code-color)',
+        link: 'var(--link-color)',
+        linkHover: 'var(--link-hover-color)',
+        selection: 'var(--selection-color)',
+        selectionText: 'var(--selection-text-color)',
       },
     },
   },
+  
   plugins: [],
 } satisfies Config;
